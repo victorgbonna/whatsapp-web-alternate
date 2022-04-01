fa02ee8d0bb2e7d9be899e1dffeaeaac54ee860b
victor ogbonna <victorgbonna@gmail.com>
2022-04-01 13:06:56 +0100

    api done

--- config/whatsapp.js
+++ config/whatsapp.js
@@ -7,37 +7,10 @@ const { Client, LocalAuth} = require('whatsapp-web.js');
 //   authStrategy: new LocalAuth()
 // });
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer:{headless: true,
+    args: ['--no-sandbox','--disable-setuid-sandbox']}
 });
 client.initialize();
 
 module.exports= client
-// client.on('qr', qr => {
-//     console.log('QR RECEIVED', qr);
-//     qrcode.generate(qr, { small: true });
-//     const qrConnect=()=>{
-//         return qr
-//     }
-// })
-// client.on('authenticated',()=>{
-//     console.log('authentication succesfully. Yes')
-// })
-// client.on('auth_failure',(message)=>{
-//     console.log('auth failure reason being that ', message)
-// })
-// client.on('ready', () => {
-//     console.log('READY OR NOT, HERE I COME!');
-//     isOnline=()=>{
-//         return true
-//     }
-// });
-// client.on('disconnected', ()=>{
-//     console.log('client is disconnected')
-//      isOnline=()=>{
-//         return false
-//     }
-// })
-
-// module.exports = {
-//   init
-// };
